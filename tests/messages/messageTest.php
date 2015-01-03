dcaf20470b6b9521b4de9708a63de3d1f5438674
Nathan Daly <justlikephp@gmail.com>
2015-01-03 14:58:34 +0000

    Update messageTest.php

--- tests/messages/messageTest.php
+++ tests/messages/messageTest.php
@@ -2,7 +2,7 @@
 
 namespace Zephlack\Tests\Messages;
 
-class MessageTest extends PHPUnit_Framework_TestCase {
+class MessageTest extends \PHPUnit_Framework_TestCase {
 
 	public function testInitWithText()
 	{
