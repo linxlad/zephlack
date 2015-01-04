43751fcfad32d941eb6aafebd4b358c653b370eb
Nathan Daly <justlikephp@gmail.com>
2015-01-04 00:17:52 +0000

    Removed undeeded files added by accident.

--- ext/zephlack/clientexception.zep.h
+++
@@ -1,5 +0,0 @@
-
-extern zend_class_entry *zephlack_clientexception_ce;
-
-ZEPHIR_INIT_CLASS(Zephlack_clientException);
-
--- ext/zephlack/http/httpexception.zep.h
+++
@@ -1,5 +0,0 @@
-
-extern zend_class_entry *zephlack_http_httpexception_ce;
-
-ZEPHIR_INIT_CLASS(Zephlack_Http_HttpException);
-
